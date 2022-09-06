9219b07a0a31d1713f28427f81bbab62281f2988
william calle <wcalleq@soen.pe>
2022-09-06 10:25:30 -0500

    fix

--- src/common/config/env.config.ts
+++ src/common/config/env.config.ts
@@ -1,6 +1,6 @@
 
 export const EnvConfiguration = () => ({
-    mongo_uri: process.env?.MONGO_URI || 'mongodb://localhost:27017/nest-pokemon',
-    port: process.env?.PORT || 3000,
-    limit_default: +process.env?.LIMIT_DEFAULT || 10
+    mongo_uri: process.env.MONGO_URI || 'mongodb://localhost:27017/nest-pokemon',
+    port: process.env.PORT || 3000,
+    limit_default: +process.env.LIMIT_DEFAULT || 10
 });
